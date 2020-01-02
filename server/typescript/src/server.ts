a183b44ffe92efd23426a64a91841906d2016f3f
Thorsten Schaeff <thorsten@stripe.com>
2020-01-02 20:39:37 +0800

    Add Typescript server.

---
+++ server/typescript/src/server.ts
@@ -0,0 +1,71 @@
+// Env variable path defined in .env.ts file.
+import "./env";
+
+import { resolve } from "path";
+
+import express from "express";
+const app = express();
+
+// Initialise Stripe with Typescript.
+import Stripe from "stripe";
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: "2019-12-03",
+  typescript: true
+});
+
+app.use(express.static(process.env.STATIC_DIR));
+// Use body-parser to retrieve the raw body as a buffer
+import bodyParser from "body-parser";
+
+app.get("/", (req, res) => {
+  const path = resolve(process.env.STATIC_DIR + "/index.html");
+  res.sendFile(path);
+});
+
+app.post("/", async (req, res) => {
+  const { data }: { data: object } = req.body;
+
+  res.send({
+    someData: data
+  });
+});
+
+app.get("/publishable-key", (req, res) => {
+  res.send({ publishableKey: process.env.STRIPE_PUBLISHABLE_KEY });
+});
+
+// Webhook handler for asynchronous events.
+app.post(
+  "/webhook",
+  bodyParser.raw({ type: "application/json" }),
+  (req, res) => {
+    let data: Stripe.Event.Data | object;
+    let eventType: string;
+
+    // Retrieve the event by verifying the signature using the raw body and secret.
+    let event: Stripe.Event;
+    const signature: string = req.headers["stripe-signature"] as string;
+
+    try {
+      event = stripe.webhooks.constructEvent(
+        req.body,
+        signature,
+        process.env.STRIPE_WEBHOOK_SECRET
+      );
+    } catch (err) {
+      console.log(`⚠️  Webhook signature verification failed.`);
+      return res.sendStatus(400);
+    }
+    // Extract the object from the event.
+    data = event.data;
+    eventType = event.type;
+
+    if (eventType === "payment_intent.succeeded") {
+      console.log(`🔔  Webhook received: ${eventType}!`);
+    }
+
+    res.sendStatus(200);
+  }
+);
+
+app.listen(4242, () => console.log(`Node server listening on port ${4242}!`));
