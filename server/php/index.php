f53af60de1ac167f90072ad9aa9e0247e089afa6
Matt Basta <basta@stripe.com>
2020-02-26 13:09:26 -0800

    Update PHP

--- server/php/index.php
+++ server/php/index.php
@@ -26,7 +26,7 @@ $app->add(function ($request, $response, $next) {
     Stripe::setApiKey(getenv('STRIPE_SECRET_KEY'));
     return $next($request, $response);
 });
-  
+
 $app->post('/onboard-user', function (Request $request, Response $response, array $args) {
 
   $account = \Stripe\Account::create([
@@ -35,17 +35,44 @@ $app->post('/onboard-user', function (Request $request, Response $response, arra
     'country' => 'US'
   ]);
 
-  $return_url = $request->getHeaderLine('Origin');
+  session_start();
+  $_SESSION['account_id'] = $account->id;
 
+  $origin = $request->getHeaderLine('Origin');
+  $account_link_url = generate_account_link($account->id, $origin);
+  
+  return $response->withJson(array('url' => $account_link_url));
+});
+
+$app->get('/onboard-user/refresh', function (Request $request, Response $response, array $args) {
+  session_start();
+  if (empty($_SESSION['account_id'])) {
+    return $response
+      ->withHeader('Location', '/')
+      ->withStatus(302);
+  }
+  $account_id = $_SESSION['account_id'];
+  
+  $origin =
+    ($_SERVER['HTTPS'] === 'on' ? 'https://' : 'http://')
+    . $request->getHeaderLine('Host');
+
+  $account_link_url = generate_account_link($account_id, $origin);
+
+  return $response
+    ->withHeader('Location', $account_link_url)
+    ->withStatus(302);
+});
+
+function generate_account_link(string $account_id, string $origin) {
   $account_link = \Stripe\AccountLink::create([
     'type' => 'onboarding',
-    'account' => $account->id,
-    'failure_url' => "{$return_url}/failure.html",
-    'success_url' => "{$return_url}/success.html"
+    'account' => $account_id,
+    'failure_url' => "{$origin}/onboard-user/refresh",
+    'success_url' => "{$origin}/success.html"
   ]);
-  
-  return $response->withJson(array('url' => $account_link->url));
-});
+  return $account_link->url;
+}
 
 $app->get('/', function (Request $request, Response $response, array $args) {   
   return $response->write(file_get_contents(getenv('STATIC_DIR') . '/index.html'));
